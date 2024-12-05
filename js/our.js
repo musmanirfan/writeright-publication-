ee8799fbe391b3cb5febf211ee6d9935443fd201
musmanirfan <usmanmala356@gmail.com>
2024-12-05 22:22:43 +0500

    index page complete

---
+++ js/our.js
@@ -0,0 +1,17 @@
+var typed = new Typed(".special", {
+    strings: [
+      "Journal Publication Assistance",
+      "Consultation",
+      "Article Writing",
+      "Manuscript Writing",
+      "Thesis Writing",
+      "Journal Editing",
+      "eBook Writing",
+      "Designing",
+      "Publishing",
+    ],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+  });
+  
